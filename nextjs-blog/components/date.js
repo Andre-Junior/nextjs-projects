cf92b7b18921703834d467a809cd7387b490c62c
Andre-Junior <andrejunior.dias@gmail.com>
2021-05-26 14:33:54 -0300

    Page posts

---
+++ nextjs-blog/components/date.js
@@ -0,0 +1,7 @@
+import { parseISO, format } from 'date-fns';
+
+
+export default function Date({dateSting}) {
+    const date = parseISO(dateSting)
+    return <time dateTime={dateSting}>{format(date, 'LLLL d, yyyy')}</time>
+}
--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -2,17 +2,20 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
-const postDirectory = path.join(process.cwd(), 'posts')
+import remark from 'remark';
+import html from 'remark-html';
+
+const postsDirectory = path.join(process.cwd(), 'posts')
 
 export function getSortedPostsData() {
     //Obter nome dos arquivos em /posts
-    const fileNames = fs.readdirSync(postDirectory)
+    const fileNames = fs.readdirSync(postsDirectory)
     const allPostsData = fileNames.map(fileName => {
         //Remover .md do nome do arquivo para obter o id
         const id = fileName.replace(/\.md$/, '')
 
         //Ler o markdown como string 
-        const fullPath = path.join(postDirectory, fileName)
+        const fullPath = path.join(postsDirectory, fileName)
         const fileContents = fs.readFileSync(fullPath, 'utf8')
 
         //Usar o gray-matter para analisar os metadata 
@@ -33,4 +36,35 @@ export function getSortedPostsData() {
             return -1
         }
     })
+}
+
+export function getAllPostIds () {
+    const fileNames = fs.readdirSync(postsDirectory)
+
+    return fileNames.map((fileName) => {
+        return {
+            params: {
+                id: fileName.replace(/\.md$/, '')
+            }
+        }
+    })
+}
+
+export async function getPostData(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+    //Usar a gray-matter para analisar o metadata
+    const matterResult = matter(fileContents)
+
+    //Usar o remark para converter markdown em HTML string
+    const processedContent = await remark().use(html).process(matterResult.content)
+    const contentHtml = processedContent.toString() 
+
+    //Combinando os dados com id e contentHtml
+    return {
+        id,
+        contentHtml,
+        ...matterResult.data
+    }
 }
